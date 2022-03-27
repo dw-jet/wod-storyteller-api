ca6a6403de299d64c55183037bf42308ff28b40d
James Teitsworth <james@jamesteitsworth.com>
2022-03-27 15:21:55 -0500

    Initial commit

---
+++ server.rb
@@ -0,0 +1,67 @@
+require "sinatra"
+require "sinatra/namespace"
+require "thin"
+
+def roll_dice(numberDice, difficulty)
+  all_rolls = []
+  numberDice.times do 
+    all_rolls.push(rand(10)+1)
+  end
+  return generate_results(all_rolls, difficulty)
+end
+
+def generate_results(rolls, difficulty)
+  ones = rolls.filter { |roll| roll == 1 }
+  successes = rolls.filter { |roll| roll >= difficulty }
+  total_successes = calculate_successes(successes, ones)
+
+  return {
+    difficulty: difficulty,
+    num_dice: rolls.count,
+    faces: rolls,
+    rolled_successes: successes.size,
+    rolled_ones: ones.size,
+    total_successes: total_successes,
+    failure: total_successes <= 0,
+    botch: total_successes < 0,
+    success: total_successes > 0
+  }
+end
+
+def calculate_successes(successes, ones)
+  successes.count - ones.count
+end
+
+get '/' do
+  'Welcome to my Old World of Darkness Dice Roller'  
+end
+
+namespace '/api/v1' do
+  before do
+    content_type 'application/json'
+  end
+
+  helpers do
+    def base_url
+      @base_url ||= "#{request.env['rack.url_scheme']}://{request.env['HTTP_HOST']}"
+    end
+
+    def json_params
+      begin
+        JSON.parse(request.body.read)
+      rescue
+        halt 400, { message:'Invalid JSON' }.to_json
+      end
+    end
+  end
+
+  get '/roll' do
+    num_dice_int = params['numDice'].to_i
+    halt(400, {message:'num_of_dice must be a positive integer'}.to_json) unless num_dice_int.integer? && num_dice_int > 0    
+    params.has_key?('difficulty') && params['difficulty'] ? difficulty = params['difficulty'].to_i : difficulty = 6
+    halt(400, {message: 'There is a problem with the difficulty you passed in. Make sure it is an integer'}.to_json) unless difficulty > 1
+    roll_dice(num_dice_int, difficulty).to_json
+  end
+end
+
+
